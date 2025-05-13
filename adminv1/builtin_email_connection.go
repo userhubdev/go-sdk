7d6cbd1055a278a6d913855396bc677aa8d514f0
Silas Sewell <silas@userhub.com>
2025-05-12 23:25:55 -0400

    Update generated code

--- adminv1/builtin_email_connection.go
+++ adminv1/builtin_email_connection.go
@@ -4,6 +4,4 @@ package adminv1
 
 // The builtin email specific connection data.
 type BuiltinEmailConnection struct {
-	// The allowed email list.
-	AllowedEmails []string `json:"allowedEmails"`
 }
---
+++ adminv1/report_user_action_response.go
@@ -0,0 +1,7 @@
+// Code generated. DO NOT EDIT.
+
+package adminv1
+
+// Response message for ReportUserAction.
+type ReportUserActionResponse struct {
+}
